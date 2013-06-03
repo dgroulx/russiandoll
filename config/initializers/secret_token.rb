f28ea066cd4d36e872702adeceefea2dae6cc546
David Groulx <david@sandbendersoftware.com>
2013-06-03 09:57:28 -0400

    Russian doll caching demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Russiandoll::Application.config.secret_key_base = '112fb2bf2a18d3ecf0a635c09d1ed1db47e8edeb7ab43e123a72febe3d484712f67946ddad17845d8ed5909ab9b19f9c6300e4dc687f183b358ce0788a852444'
