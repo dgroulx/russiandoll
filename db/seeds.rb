406082a41b722ee6ec310e7a1a1595bbe3e7528a
David Groulx <david@sandbendersoftware.com>
2013-06-03 16:07:02 -0400

    Style updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 User.destroy_all
+Post.destroy_all
+Comment.destroy_all
 
 (1..10).each do |i|
   u = User.create(name: "#{Faker::Name.first_name} #{Faker::Name.last_name}", blog_name: Faker::Lorem.sentence)
   (1..10).each do |j|
-    p = u.posts.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
+    p = u.posts.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraphs.join('\n'))
     (1..10).each do |k|
       p.comments.create(content: Faker::Lorem.paragraph)
     end
